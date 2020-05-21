dbc0c99fc5d6f8cb0a58bf6accbe7d371ddf7125
Adewole Adewumi <wolejr@gmail.com>
2020-05-20 23:08:48 -0400

    Restyled home page and created Services page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,67 +1,108 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {
   Link
 } from 'react-router-dom';
+import Carousel from 'react-bootstrap/Carousel';
 
-export default class Home extends Component {
-  constructor(){
-    super();
-    this.state = {
-      name: "Hodil"
-    };
-  }
+export default function Home() {
+  const [index, setIndex] = useState(0);
 
-  render(){
-    return (
-      <div className="container">
-          <div className="row">
-              <div className="col-md-1 social-links">
-                  <div className="round-btn">
-                    <div className="icon">
-                      <i className="fa fa-instagram" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="round-btn">
-                    <div className="icon">
-                      <i className="fa fa-vimeo" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="round-btn">
-                    <div className="icon">
-                      <i className="fa fa-twitter" aria-hidden="true"></i>
-                    </div>
-                  </div>
-                  <div className="round-btn">
-                    <div className="icon">
-                      <i className="fa fa-facebook" aria-hidden="true"></i>
-                    </div>
-                  </div>
-              </div>
-              <div className="col-md-5 info-section">
-                  <div className="round-bar black-btn">Established since 2012</div>
-                  <h1>We do big things<br/> with big ideas.</h1>
-                  <p>
-                      You can choose one of our demos. Easy to import and <br/>
-                      highly customizable. You will find what you need in one <br/>
-                      of our fully equipped demos.
-                  </p>
-                  <div className="buttons">
-                      <Link className="round-bar yellow-btn" to="/about">About Us</Link>
-                      <Link className="round-bar blue-btn" to="/services">Services</Link>
-                  </div>
-              </div>
-              <div className="col-md-6">
-                <div className="featured-project">
-                  <img src="https://cdn.vox-cdn.com/thumbor/F3EXGcvCo9QPs5oS9OvbxLV9-7c=/0x0:4761x2970/1200x800/filters:focal(1639x1407:2399x2167)/cdn.vox-cdn.com/uploads/chorus_image/image/65375048/3.0.jpg" className="img-fluid image-bg" alt="New sky scraper completed in January 2020" />
-                  <div className="latest-project bg-dark">
-                    <div className="highlight">Latest Project</div>
-                    <div className="title text-light">Oxford Science Lab building</div>
-                    <p>Ottawa, ON</p>
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+  
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-1 social-links">
+                <div className="round-btn">
+                  <div className="icon">
+                    <i className="fa fa-instagram" aria-hidden="true"></i>
                   </div>
                 </div>
+                <div className="round-btn">
+                  <div className="icon">
+                    <i className="fa fa-vimeo" aria-hidden="true"></i>
+                  </div>
+                </div>
+                <div className="round-btn">
+                  <div className="icon">
+                    <i className="fa fa-twitter" aria-hidden="true"></i>
+                  </div>
+                </div>
+                <div className="round-btn">
+                  <div className="icon">
+                    <i className="fa fa-facebook" aria-hidden="true"></i>
+                  </div>
+                </div>
+            </div>
+            <div className="col-md-5 info-section">
+                <div className="round-bar black-btn">Established since 2012</div>
+                <h1>We do big things<br/> with big ideas.</h1>
+                <p>
+                    You can choose one of our demos. Easy to import and <br/>
+                    highly customizable. You will find what you need in one <br/>
+                    of our fully equipped demos.
+                </p>
+                <div className="buttons">
+                    <Link className="round-bar yellow-btn" to="/about">About Us</Link>
+                    <Link className="round-bar blue-btn" to="/services">Services</Link>
+                </div>
+            </div>
+            <div className="col-12 col-md-6">
+              <div className="featured-project">
+                <Carousel activeIndex={index} onSelect={handleSelect}>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src="https://wilden.ca/wp-content/uploads/2018/06/homeplans-1-half-storey-card-600x400.jpg"
+                      alt="First slide"
+                    />
+                    <Carousel.Caption>
+                      <div className="slide-text">
+                        <h3>Somerset Residence</h3>
+                        <p>White Rock, BC</p>
+                      </div>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src="https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_standard/public/images/15/09/front.jpg?itok=9ZqRQo6l"
+                      alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                      <div className="slide-text">
+                        <h3>Carleton Heights</h3>
+                        <p>Ottawa, ON</p>
+                      </div>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                  <Carousel.Item>
+                    <img
+                      className="d-block w-100"
+                      src="https://cdn.onekindesign.com/wp-content/uploads/2020/05/Farmhouse-Style-Home-Dana-Webber-Design-Group-01-1-Kindesign-600x400.jpg"
+                      alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                      <div className="slide-text">
+                        <h3>Alta Vista</h3>
+                        <p>
+                          Ottawa, ON
+                        </p>
+                      </div>
+                    </Carousel.Caption>
+                  </Carousel.Item>
+                </Carousel>
+                  <div className="latest-project bg-dark">
+                    <div className="highlight">Latest Projects</div>
+                  </div>
               </div>
-          </div>
-      </div>
-    );
-  }
+            </div>
+        </div>
+    </div>
+  );
+  
 }
--- src/components/Services.js
+++ src/components/Services.js
@@ -1,16 +1,85 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Services extends Component {
+export default function Services() {
+  
+  return (
+    <div className="container">
+        <div className="row pt-4">
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={{height: '225px', width: '100%', display: 'block'}} src="https://reliantconstruction.net/wp-content/uploads/2017/08/home-blueprints.jpg" data-holder-rendered="true"/>
+                <div className="card-body">
+                  <h3 className="font-weight-bold">Architectural Design</h3>
+                  <p className="card-text mt-4 text-dark">
+                    We go beyond standard investigative methods and procedures with state of the art technologies to ensure accurate reporting, lower costs, and side-step unnecessary intrusive testing methods.
+                  </p>
+                </div>
+              </div>
+            </div>
+            
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={{height: '225px', width: '100%', display: 'block'}} src="https://galbern.com/wp-content/uploads/2018/12/gabern-construction-works-33.jpg" data-holder-rendered="true"/>
+                <div className="card-body">
+                  <h3 className="font-weight-bold">Construction Works</h3>
+                  <p className="card-text mt-4 text-dark">
+                    We go beyond standard investigative methods and procedures with state of the art technologies to ensure accurate reporting, lower costs, and side-step unnecessary intrusive testing methods.
+                  </p> 
+                </div>
+              </div>
+            </div>
 
-  render(){
-    return (
-      <div className="container">
-          <div className="row">
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={{height: '225px', width: '100%', display: 'block'}} src="https://www.farrellmovers.com/wp-content/uploads/2019/11/House-Renovation.jpg" data-holder-rendered="true"/>
+                <div className="card-body">
+                  <h3 className="font-weight-bold">House Renovation</h3>
+                  <p className="card-text mt-4 text-dark">
+                    We go beyond standard investigative methods and procedures with state of the art technologies to ensure accurate reporting, lower costs, and side-step unnecessary intrusive testing methods.
+                  </p>
+                  
+                </div>
+              </div>
+            </div>
 
-            <div className="col">Services Page</div>
-              
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={{height: '225px', width: '100%', display: 'block'}} src="https://www.latestquality.com/wp-content/uploads/2018/03/quality-assurance-in-construction.jpg" data-holder-rendered="true"/>
+                <div className="card-body">
+                  <h3 className="font-weight-bold">Quality Assurance</h3>
+                  <p className="card-text mt-4 text-dark">
+                    We go beyond standard investigative methods and procedures with state of the art technologies to ensure accurate reporting, lower costs, and side-step unnecessary intrusive testing methods.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={{height: '225px', width: '100%', display: 'block'}} src="https://www.urbanmark.ee/wp-content/uploads/2016/10/landscape-architecture-example.jpg" data-holder-rendered="true"/>
+                <div className="card-body">
+                  <h3 className="font-weight-bold">Landscaping</h3>
+                  <p className="card-text mt-4 text-dark">
+                    We go beyond standard investigative methods and procedures with state of the art technologies to ensure accurate reporting, lower costs, and side-step unnecessary intrusive testing methods.
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card mb-4 box-shadow">
+                <img className="card-img-top" data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" alt="Thumbnail [100%x225]" style={{height: '225px', width: '100%', display: 'block'}} src="https://www.no9.ca/wp-content/uploads/2017/11/GreenHouse.jpg" data-holder-rendered="true"/>
+                <div className="card-body">
+                  <h3 className="font-weight-bold">Sustainability</h3>
+                  <p className="card-text mt-4 text-dark">
+                    We go beyond standard investigative methods and procedures with state of the art technologies to ensure accurate reporting, lower costs, and side-step unnecessary intrusive testing methods.
+                  </p>
+                </div>
+              </div>
+            </div>
+
           </div>
-      </div>
-    );
-  }
+    </div>
+  );
+  
 }
